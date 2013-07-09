bab71059f3a130fe34a4dcbbcf2de4048983874a
Eric Bréchemier <eric@bezen.org>
2013-07-09 14:27:25 +0200

    Add alias() function to define an alias of (Native prototype) functions
    The alias function is then called with the context object as first
    parameter followed with other parameters.
    
    Surfing on bind().

---
+++ alias.js
@@ -0,0 +1,20 @@
+/*
+  Define an alias for a (Native prototype) function
+
+  The alias allows to call the function with the context object
+  as first argument, followed with regular arguments of the function.
+
+  Dependency:
+    nada/bind.js
+
+  Example:
+    var has = alias( Object.prototype.hasOwnProperty );
+    has( object, name ) === object.hasOwnProperty( name ); // true
+
+  Parameter:
+    func - function, a method part of the prototype of a Constructor
+*/
+function alias( func ) {
+  return bind( func.call, func );
+}
+
